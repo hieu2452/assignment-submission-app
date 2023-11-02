2d3e0eb8874e8626ce87d96fd3e33c6862968682
hieu2452 <hieu245202@gmail.com>
2023-11-02 23:39:47 +0700

    front end add interceptor jwt

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Frontend';
+  constructor(private userService: UserService) {
+
+  }
+  setCurrentUser() {
+    const userSring = localStorage.getItem('user');
+    if (!userSring) return;
+
+    const user = JSON.parse(userSring);
+    this.userService.setCurrentUser(user);
+
+  }
 }
--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { SignupComponent } from '../signup/signup.component';
 import { LoginComponent } from '../login/login.component';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { take } from 'rxjs/operators';
+import { log } from 'console';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,20 +13,49 @@ import { LoginComponent } from '../login/login.component';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private dialog:MatDialog) { }
+  user = {};
+
+  constructor(private dialog: MatDialog, private router: Router, private userSerivce: UserService) {
+    this.userSerivce.currentUser$.pipe(take(1)).subscribe({
+      next: currentUser => {
+        if (currentUser) {
+          this.user = currentUser
+          console.log(currentUser)
+        }
+      }
+    })
+
+  }
 
   ngOnInit(): void {
+    this.checkLogin();
+    console.log('siu')
   }
 
-  handleSignupAction(){
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.width="550px";
-    this.dialog.open(SignupComponent,dialogConfig);
-  }
-
-  handleLoginAction(){
+  handleSignupAction() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.width = "550px";
-    this.dialog.open(LoginComponent , dialogConfig)
+    this.dialog.open(SignupComponent, dialogConfig);
+  }
+
+  handleLoginAction() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = "550px";
+    this.dialog.open(LoginComponent, dialogConfig)
+  }
+
+  // checkLogin() {
+  //   if (!this.user) return;
+
+  //   this.router.navigate(['/cafe/dashboard']);
+  // }
+
+  checkLogin() {
+    const userSring = localStorage.getItem('user');
+    if (!userSring) return;
+    this.router.navigate(['/cafe/dashboard']);
+    const user = JSON.parse(userSring);
+    this.userSerivce.setCurrentUser(user);
+    
   }
 }
