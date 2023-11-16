9d087fe31406b1e6b7ec4e3a39451e5f51d8a82d
Hoàng Super Đặng <hdang3135@gmail.com>
2023-11-16 22:31:13 +0700

    done bill

--- Frontend/src/app/material-component/dialog/view-bill-products/view-bill-products.component.ts
+++ Frontend/src/app/material-component/dialog/view-bill-products/view-bill-products.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { BillService } from 'src/app/services/bill.service';
 import { SnackbarService } from 'src/app/services/snackbar.service';
 import { ConfirmationComponent } from '../confirmation/confirmation.component';
 import { MatTableDataSource } from '@angular/material/table';
 import { GlobalConstants } from 'src/app/shared/global-constants';
+import { log } from 'console';
 
 @Component({
   selector: 'app-view-bill-products',
@@ -14,10 +15,22 @@ import { GlobalConstants } from 'src/app/shared/global-constants';
 })
 export class ViewBillProductsComponent implements OnInit {
 
-  constructor() { }
+  dataplayedColumns: string[] = ['name', 'price', 'quantity', 'total'];
+  dataSource: any;
+  data: any;
+  totalAmount: number = 0;
 
-    ngOnInit(): void {
-    
+  constructor(@Inject(MAT_DIALOG_DATA) public dialogData: any,
+    public dialogRef: MatDialogRef<ViewBillProductsComponent>) { }
+
+  ngOnInit() {
+    this.data = this.dialogData.data;
+    this.dataSource = new MatTableDataSource(this.dialogData.data.products);
+    console.log(this.data);
+    let total_amount = 0;
+    for(let product of this.data.products) {
+      total_amount += product.quantity * product.price;
     }
-    
+    this.totalAmount = total_amount;
+  }
 }
